2dc26908fa222df7d38daed9bd5b1c3b8350007f
Wendel Duarte <wendel.duarte@proton.me>
2026-05-11 03:20:45 -0300

    created inspection interface

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)();
 const refSKUForm = document.getElementById("ref-sku-form");
 const refSKUInput = document.getElementById("ref-sku-input");
 const refSKUMsg = document.getElementById("ref-sku-msg");
+const refSKUDisplay = document.getElementById("ref-sku-display");
+const newBarcodeForm = document.getElementById("new-barcode-form");
+const newBarcodeInput = document.getElementById("new-barcode-input");
+const boxCounter = document.getElementById("box-counter");
 
 function clickySound(time, freq) {
   const o = ctx.createOscillator();
@@ -21,22 +25,43 @@ function clickySound(time, freq) {
   o.stop(time + 0.03);
 }
 
-function errorSound(time) {
+function warnSound(time) {
   const o = ctx.createOscillator();
   const g = ctx.createGain();
 
-  o.type = "square";
+  o.type = "triangle";
 
-  o.frequency.setValueAtTime(200, time);
-  o.frequency.exponentialRampToValueAtTime(50, time + 0.05);
+  o.frequency.setValueAtTime(557, time);
+  o.frequency.exponentialRampToValueAtTime(257, time + 0.025);
+  o.frequency.exponentialRampToValueAtTime(357, time + 0.3);
 
-  g.gain.setValueAtTime(0.1, time);
-  g.gain.exponentialRampToValueAtTime(0.03, time + 0.05);
+  g.gain.setValueAtTime(0.5, time);
+  g.gain.exponentialRampToValueAtTime(0.3, time + 0.025);
+  g.gain.exponentialRampToValueAtTime(0.00001, time + 0.3);
 
   o.connect(g).connect(ctx.destination);
 
   o.start();
   o.stop(time + 0.3);
+}
+
+function errorSound(time) {
+  const o = ctx.createOscillator();
+  const g = ctx.createGain();
+
+  o.type = "sawtooth";
+
+  o.frequency.setValueAtTime(82.41, time);
+  o.frequency.setValueAtTime(77.78, time + 0.21);
+
+  g.gain.setValueAtTime(0.1, time);
+  g.gain.setValueAtTime(0.00001, time + 0.1);
+  g.gain.setValueAtTime(0.1, time + 0.2);
+
+  o.connect(g).connect(ctx.destination);
+
+  o.start();
+  o.stop(time + 0.4);
 }
 
 function blipSound(time) {
@@ -79,12 +104,12 @@ function validateSKU(barcode) {
     return "A SKU é obrigatória";
   }
 
-  if (!pattern.test(barcode)) {
-    return "Escaneie uma SKU válida";
-  }
-
   if (barcode.length < 13 || barcode.length > 13) {
     return "A SKU deve ter exatamente 13 caracteres";
+  }
+
+  if (!pattern.test(barcode)) {
+    return "Escaneie uma SKU válida";
   }
 
   return null;
@@ -100,11 +125,28 @@ function resetSKUInput() {
     "Escaneie a <span class='bold'>SKU</span> de qualquer caixa do pallet para iniciar a conferência.";
 }
 
+function setReferenceSKU(code) {
+  localStorage.setItem("referenceSKU", code);
+  refSKUDisplay.innerHTML = code;
+}
+
+function clearReferenceSKU() {
+  localStorage.removeItem("referenceSKU");
+  refSKUDisplay.innerHTML = "";
+}
+
+function setBoxCounter(number) {
+  localStorage.setItem("boxCount", number);
+  boxCounter.innerHTML = number;
+}
+
+function clearBoxCounter() {
+  localStorage.removeItem("boxCount");
+  boxCounter.innerHTML = "";
+}
+
 // #init-prompt
 document.getElementById("new-inspect-btn").addEventListener("click", () => {
-  const now = ctx.currentTime;
-
-  blipSound(now);
   resetSKUInput();
 
   showElements(["set-sku-sect"]);
@@ -144,6 +186,8 @@ refSKUForm.addEventListener("submit", async (e) => {
     refSKUInput.classList.add("green-input");
     refSKUMsg.classList.add("green");
     refSKUMsg.innerHTML = "Aceito";
+    newBarcodeInput.value = "";
+    newBarcodeInput.placeholder = "Leia um código de barras...";
 
     await ctx.resume();
 
@@ -153,11 +197,43 @@ refSKUForm.addEventListener("submit", async (e) => {
     clickySound(now + 0.12, 1700);
     clickySound(now + 0.2, 2300);
 
-    localStorage.setItem("referenceSKU", SKU);
+    setReferenceSKU(SKU);
+    setBoxCounter(1);
 
     setTimeout(() => {
       hideElements(["set-sku-sect"]);
       resetSKUInput();
+      showElements(["inspection-interface-sect"]);
     }, 1500);
   }
+});
+
+newBarcodeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const currentReference = localStorage.getItem("referenceSKU");
+  const barcode = e.target.elements[0].value;
+  const now = ctx.currentTime;
+
+  const error = validateSKU(barcode);
+
+  if (error) {
+    warnSound(now);
+
+    newBarcodeInput.value = "";
+    newBarcodeInput.placeholder = error;
+
+    return;
+  } else if (barcode !== currentReference) {
+    errorSound(now);
+
+    newBarcodeInput.value = "";
+    newBarcodeInput.placeholder = "A SKU é diferente da referência";
+
+    return;
+  }
+
+  blipSound(now);
+  setBoxCounter(Number(localStorage.getItem("boxCount")) + 1);
+  newBarcodeInput.value = "";
 });
